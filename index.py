b46acfe4e2490d3f1da6b6ded7f132545329f8a5
root <root@LovelyJeno.localdomain>
2023-03-25 08:19:15 +0700

    index.html

--- index.py
+++ index.py
@@ -4,10 +4,10 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications.efficientnet import preprocess_input
+import PIL
 
 app = Flask(__name__)
 
-# age = {0: '7', 1: '8', 2: '9', 3: '10', 4: '11', 5: '12', 6: '13', 7: '14', 8: '15', 9: '16', 10: '17', 11: '18', 12: '19', 13: '20', 14: '21', 15: '22', 16: '23', 17: '24', 18: '25'}
 gender = {0: 'Female', 1: 'Male'}
 
 import sys
@@ -29,62 +29,62 @@ model1.make_predict_function()
 
 sys.path.append('/root/WebApp/Web_app/templates/Gender.h5')
 
-from efficientnet.layers import Swish, DropConnect
-from efficientnet.model import ConvKernalInitializer
-from tensorflow.keras.utils import get_custom_objects
-
 get_custom_objects().update({
     'ConvKernalInitializer': ConvKernalInitializer,
     'Swish': Swish,
     'DropConnect':DropConnect
 })
+
 model2 = tf.keras.models.load_model('/root/WebApp/Web_app/templates/Gender.h5')
-
-
 model2.make_predict_function()
 
-# def predict_image1(img_path):
-#     # Read the image and preprocess it
-#     img = image.load_img(img_path, target_size=(150, 150))
-#     x = image.img_to_array(img)
-#     x = preprocess_input(x)
-#     x = np.expand_dims(x, axis=0)
-#     result = model1.predict(x)
-#     return age[result.argmax()]
-
-# def predict_image2(img_path):
-#     # Read the image and preprocess it
-#     img = image.load_img(img_path, target_size=(150, 150))
-#     g = image.img_to_array(img)
-#     g = preprocess_input(g)
-#     g = np.expand_dims(g, axis=0)
-#     result = model2.predict(g)
-#     return gender[result.argmax()]
-# my_tuple = tuple(age)
-
-def predict_image1(img_path):
+def predict_Age1(Rt):
     # Read the image and preprocess it
-    img = image.load_img(img_path, target_size=(150, 150))
+    img = image.load_img(Rt, target_size=(150, 150))
     x = image.img_to_array(img)
     x = x.reshape((1,) + x.shape) 
     x /= 255.
     result = model1.predict(x)
     return np.around(result[0]+6, 2)
 
-def predict_image2(img_path):
+def predict_Gender1(Rt):
     # Read the image and preprocess it
-    img = image.load_img(img_path, target_size=(150, 150))
+    img = image.load_img(Rt, target_size=(150, 150))
     g = image.img_to_array(img)
-    g = g.reshape((1,) + g.shape) 
+    g = g.reshape((1,) + g.shape)
     g /= 255.
     result = model2.predict(g)
+    print(result[0])
+    print(result.argmax())
     return gender[result.argmax()]
+
+def predict_Age2(Lt):
+    # Read the image and preprocess it
+    img = image.load_img(Lt, target_size=(150, 150))
+    x = image.img_to_array(img)
+    x = x.reshape((1,) + x.shape) 
+    x /= 255.
+    result = model1.predict(x)
+    return np.around(result[0]+6, 2)
+
+
+def predict_Gender2(Lt):
+    img = image.load_img(Lt, target_size=(150, 150))
+    g = image.img_to_array(img)
+    g = g.reshape((1,) + g.shape)
+    g /= 255.
+    result = model2.predict(g)
+    print(result[0])
+    print(result.argmax())
+    return gender[result.argmax()]
+    
+
 
 
 # routes
 @app.route('/')
 def index():
-    return render_template('upload.html')
+    return render_template('upload2.html')
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
@@ -93,14 +93,58 @@ def upload():
         file = request.files['image']
         img_path = 'static/panoramic.jpg'
         file.save(img_path)
-  
-        # Predict the age
 
-        age_pred = predict_image1(img_path)
-        gender_pred = predict_image2(img_path)
+        frac = 0.60
+        filename = img_path.split("/")[-1]
+
+        # Read the image and create cropped versions
+        img = Image.open(img_path)
+
+        #left---------------------------------------------------
+        left = 0
+        upper = 0
+        right = img.size[0]-((1-frac))*img.size[0] 
+        bottom = img.size[1]
+        cropped_left = img.crop((left, upper, right, bottom))
+        #save setting by youreself'
+        Rt = 'static/Rt_panoramic.jpg'
+        cropped_left.save(Rt)
+
+
+        #right ---------------------------------------------------
+        left = img.size[0]*((1-frac))
+        upper = 0
+        right = img.size[0]
+        bottom = img.size[1]
+        cropped_right= img.crop((left, upper, right, bottom))
+        #save setting by youreself
+        cropped_right.save('static/unflip_'+filename)
+
+        #flip ---------------------------------------------------
+        #read the image
+        im = Image.open('static/unflip_'+filename)
+
+        #flip image
+        out = im.transpose(PIL.Image.FLIP_LEFT_RIGHT)
+        Lt = 'static/Flip_panoramic.jpg'
+        out.save(Lt)
+
+        # Predict the age
+        # Predict the age
+        age_pred = predict_Age1(Rt)+predict_Age2(Lt)/2
+        age_pred1 = np.around(age_pred[0], 2)
+
+        gender_pred1 = predict_Gender1(Rt)
+        gender_pred2 = predict_Gender2(Lt)
+
+        if gender_pred1 > gender_pred2:
+            gender_pred = gender_pred1
+        else:
+            gender_pred = gender_pred2
 
         # Render the prediction result
-        return render_template('upload_completed.html', prediction1=age_pred[0], prediction2=gender_pred)
+        return render_template('upload_completed.html', prediction1=age_pred1, prediction2=gender_pred)
 
+  
 if __name__ == '__main__':
     app.run(debug=True)
